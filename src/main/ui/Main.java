b24517ed75fd6f64a63e1dac3f05cfa736226177
Amy Xiong <axiong02@students.cs.ubc.ca>
2023-02-10 10:51:55 -0800

    Added more tests in logTest.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,4 +5,5 @@ public class Main {
         WordleApp wordle = new WordleApp();
         wordle.runWordle();
     }
+
 }
--- src/test/model/LogTest.java
+++ src/test/model/LogTest.java
@@ -12,5 +12,8 @@ public class LogTest {
     }
 
     @Test
+    public void testAnalyzeListOfGuesses() {
+        //stub
+    }
 
 }
