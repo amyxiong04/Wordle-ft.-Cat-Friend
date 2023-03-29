1b41540505112ce6b703ecb0a4ae27a7fe0929d9
Amy Xiong <axiong02@students.cs.ubc.ca>
2023-03-29 13:24:08 -0700

    Final Phase 3 Submission.

--- src/main/ui/MainConsole.java
+++ src/main/ui/MainConsole.java
@@ -3,7 +3,7 @@ package ui;
 import java.util.Scanner;
 
 // Initializes the console wordle application, allowing the user to play again if they wish
-public class Main {
+public class MainConsole {
     public static void main(String[] args) {
         Boolean userWantsToPlayAgain = null;
         do {
